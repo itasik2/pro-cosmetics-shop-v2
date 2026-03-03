c5b316f423ba691eafba42134d056555c24a358b
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-03-03 08:49:26 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ function WhatsAppIcon({ className }: { className?: string }) {
     <svg
       aria-hidden="true"
       viewBox="0 0 32 32"
-      className={className || "w-4 h-4"}
+      className={className || "w-6 h-6"}
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
     >
