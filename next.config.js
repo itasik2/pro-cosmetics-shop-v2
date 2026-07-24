5c3d197e0c5990d9f8bbb1521d2d93464767570a
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-07-24 19:42:20 +0500

    Исправить загрузку PDF.js worker на Vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['*'] } }
+  experimental: {
+    serverActions: { allowedOrigins: ["*"] },
+    serverComponentsExternalPackages: ["pdfjs-dist"],
+    outputFileTracingIncludes: {
+      "/api/admin/price-imports/upload": [
+        "./node_modules/pdfjs-dist/legacy/build/**/*",
+      ],
+    },
+  },
 };
+
 module.exports = nextConfig;
