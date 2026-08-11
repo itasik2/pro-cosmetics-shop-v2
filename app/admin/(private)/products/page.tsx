7992647f373a1c0d53746863a5c39405e21e5acd
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-08-11 16:16:16 +0500

    Показать объединение дублей в админке товаров

--- app/admin/(private)/products/page.tsx
+++ app/admin/(private)/products/page.tsx
@@ -1,5 +1,6 @@
 import AdminProductsClient from "./AdminProductsClient";
 import DraftProductsPublisher from "./DraftProductsPublisher";
+import VariantMergeMaintenance from "./VariantMergeMaintenance";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -8,6 +9,7 @@ export default function Page() {
   return (
     <div className="space-y-6">
       <DraftProductsPublisher />
+      <VariantMergeMaintenance />
       <AdminProductsClient />
     </div>
   );
