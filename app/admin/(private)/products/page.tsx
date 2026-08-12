43387597649fe1926d001d02bd54097f0b9c734c
itasik2 <125400680+itasik2@users.noreply.github.com>
2026-08-12 11:13:40 +0500

    Перенести поиск товаров наверх страницы

--- app/admin/(private)/products/page.tsx
+++ app/admin/(private)/products/page.tsx
@@ -1,3 +1,4 @@
+import AdminProductSearchBar from "./AdminProductSearchBar";
 import AdminProductsClient from "./AdminProductsClient";
 import DraftProductsPublisher from "./DraftProductsPublisher";
 import VariantMergeMaintenance from "./VariantMergeMaintenance";
@@ -7,7 +8,8 @@ export const revalidate = 0;
 
 export default function Page() {
   return (
-    <div className="space-y-6">
+    <div className="space-y-4">
+      <AdminProductSearchBar />
       <DraftProductsPublisher />
       <VariantMergeMaintenance />
       <AdminProductsClient />
